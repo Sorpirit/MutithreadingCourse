31ba2c329b5c8f56b56f791ab95d1ed575ccc937
Sorpirit <danvuk11@gamil.com>
2023-02-24 20:13:47 +0100

    First task. Add code from code example

--- BouncingBallsDemo/src/Main.java
+++ BouncingBallsDemo/src/Main.java
@@ -1,5 +1,12 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BounceFrame frame = new BounceFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setVisible(true);
+        System.out.println("Thread name = " +
+                Thread.currentThread().getName());
     }
 }
